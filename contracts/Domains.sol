84445fe0ea23159d77e445e3c4dafe6b3a576d1c
Sebastian Itokazu <sebitokazu@gmail.com>
2022-02-19 20:26:35 -0300

    Payable domain

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
+import { StringUtils } from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
+
+    string public tld;
+
     mapping(string => address) domains;
     mapping(address => string) inverseDomain;
     mapping(string => Record) records;
@@ -14,18 +18,37 @@ contract Domains {
         string twitter;
     }
 
-    constructor() {
-        console.log("IBIS domains");
+    constructor(string memory _tld) payable {
+        tld = _tld;
+        console.log(".%s domain service contract deployed", _tld);
     }
 
-    function register(string calldata _name) public {
+      // This function will give us the price of a domain based on length
+    function price(string calldata name) public pure returns(uint) {
+        uint len = StringUtils.strlen(name);
+        require(len > 0, "Domain can't be blank");
+        if (len == 3) {
+            return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+        } else if (len == 4) {
+            return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
+        } else {
+            return 1 * 10**17;
+        }
+    }
+
+    function register(string calldata _name) public payable{
         require(domains[_name] == address(0), "Domain is already registered");
-        require(keccak256(abi.encodePacked(_name)) != keccak256(abi.encodePacked("")), "Domain can't be blank");
+
+        uint _price = price(_name);
+
+        // Check if enough Matic was paid in the transaction
+        require(msg.value >= _price, "Not enough MATIC paid");
+        
         domains[_name] = msg.sender;
         inverseDomain[msg.sender] = _name;
         records[_name] = Record('','','');
 
-        console.log("%s has registered the domain %s", msg.sender, _name);
+        console.log("%s has registered the domain %s.%s", msg.sender, _name, tld);
     }
 
     function getAddress(string calldata _name) public view returns (address) {
--- scripts/run.js
+++ scripts/run.js
@@ -1,26 +1,29 @@
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
-  const domainContract = await domainContractFactory.deploy();
+  const domainContract = await domainContractFactory.deploy('ibis');
   await domainContract.deployed();
   console.log("Contract deployed to:", domainContract.address);
   console.log("Contract deployed by:", owner.address);
 
-  let txn = await domainContract.register("itoka");
+  let txn = await domainContract.register("itoka", {value: hre.ethers.utils.parseEther('0.1')});
   await txn.wait();
 
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+
   const domainOwner = await domainContract.getAddress("itoka");
-  console.log("Owner of domain:", domainOwner);
+  console.log("Owner of domain itoka:", domainOwner);
 
   const addressDomain = await domainContract.getDomain(owner.address);
-  console.log("Domain for address:", addressDomain);
+  console.log("Domain for %s:%s",domainContract.address,addressDomain);
 
 
   txn = await domainContract.setAllRecords("itoka", "Itoka", "https://open.spotify.com/track/5Y0hkLkzdrTuPGWYLvm6oO?si=04a4fc609d35498a","seba_itokazu");
   await txn.wait();
 
   const domainRecords = await domainContract.getRecord('itoka');
-  console.log("Record of domain:", domainRecords);
+  console.log("Record of domain itoka:", domainRecords);
 };
 
 const runMain = async () => {
